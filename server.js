43a9794debb3d29f29e05d2b122da9ef1dbd2331
Andrey Chorniy <creatuseandr@icloud.com>
2022-06-26 20:57:49 +0300

    connect middleware to application and create  connection to mongoose database

--- server.js
+++ server.js
@@ -7,6 +7,15 @@ require("dotenv").config();
 
 const port = process.env.PORT || 5000;
 
-app.get("/", (req, res) => {
-  res.send("hello guys");
+app
+  .use(express.json())
+  .use(express.urlencoded({ extended: true }))
+  .use("/static", express.static(__dirname + "/assets"))
+  .use("/api/planes", require("./routes/planes"));
+
+/* Connecting to the mongodb database and then starting the server. */
+mongoose.connect("mongodb://127.0.0.1:27017").then(() => {
+  app.listen(port, () => {
+    console.log(`server was started at port ${port}`);
+  });
 });
