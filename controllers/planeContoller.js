8d86091cbe7d2c4bb4b70f147a55cbae4cefbda2
Andrey Chorniy <creatuseandr@icloud.com>
2022-06-27 13:39:21 +0300

    created server side validation

--- controllers/planeContoller.js
+++ controllers/planeContoller.js
@@ -14,6 +14,38 @@ const getPlanes = async (req, res) => {
 };
 
 const createPlane = async (req, res) => {
+  const errors = {};
+
+  if (!req.body.name) {
+    errors.name = { message: "Пожалуйста, укажите имя!" };
+  }
+
+  if (!req.body.price) {
+    errors.price = { message: "Пожалуйста, укажите цену!" };
+  }
+
+  if (!req.body.description) {
+    errors.description = {
+      message: "Пожалуйста, укажите описание!",
+    };
+  }
+
+  if (!req.body.capacity) {
+    errors.capacity = {
+      message: "Пожалуйста, укажите вместимость!",
+    };
+  }
+
+  if (!req.file) {
+    errors.planeImage = {
+      message: "Добавьте фото самолета!",
+    };
+  }
+
+  if (Object.keys(errors).length > 0) {
+    return res.status(400).json(errors);
+  }
+
   try {
     const { name, price, description, capacity } = req.body;
 
