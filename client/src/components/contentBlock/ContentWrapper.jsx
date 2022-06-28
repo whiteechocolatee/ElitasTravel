3979a086f219ba10b66919f0ef90adeb22238305
Andrey Chorniy <creatuseandr@icloud.com>
2022-06-28 16:44:41 +0300

    deleted unusable code from components

--- client/src/components/contentBlock/ContentWrapper.jsx
+++ client/src/components/contentBlock/ContentWrapper.jsx
@@ -2,17 +2,9 @@ import React from "react";
 import styles from "./contentWrapper.module.css";
 
 function ContentWrapper({ children, className }) {
-  console.log(
-    "children array >>>>",
-    children[0].props.children,
-  );
-  console.log("class >>>", className);
   return (
-    <div
-      className={`${styles.wrapper} ${className}`}>
-      {children.map((item) => {
-        return <p>{item.props.children}</p>;
-      })}
+    <div className={`${styles.wrapper} ${className}`}>
+      {children}
     </div>
   );
 }
