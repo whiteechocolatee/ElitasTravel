a41d883b88f0b3cbbb66cea9f9899bda798f48bb
Andrey Chorniy <creatuseandr@icloud.com>
2022-06-28 18:29:21 +0300

    created new component and connect to the homepage

---
+++ client/src/components/planes/Planes.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Planes() {
+  return <div>Planes</div>;
+}
+
+export default Planes;
--- client/src/pages/Homepage/Homepage.jsx
+++ client/src/pages/Homepage/Homepage.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "../../components/header/Header";
+import Planes from "../../components/planes/Planes";
 
 function Homepage() {
   return (
     <React.Fragment>
       <Header />
+      <Planes />
     </React.Fragment>
   );
 }
