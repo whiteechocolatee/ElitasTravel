06cdddbf23e14bffab484d98698c4104fdce1651
Andrey Chorniy <creatuseandr@icloud.com>
2022-06-28 20:45:20 +0300

    took planes arr from state and showing at planes page , create useEffect hook for sending request while page is loading

--- client/src/components/planes/Planes.jsx
+++ client/src/components/planes/Planes.jsx
@@ -1,7 +1,26 @@
 import React from "react";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getPlanes } from "../../store/plane/planeSlice";
+import Spinner from "../loader/Spinner";
 
 function Planes() {
-  return <div>Planes</div>;
+  const dispatch = useDispatch();
+  const { planes } = useSelector((state) => {
+    return state.planes;
+  });
+
+  console.log("planes >>>", planes);
+
+  useEffect(() => {
+    dispatch(getPlanes());
+  }, [dispatch]);
+
+  return (
+    <div>
+      <Spinner />
+    </div>
+  );
 }
 
 export default Planes;
