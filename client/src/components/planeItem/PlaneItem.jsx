341a81274b533b45f2c6dad805197bf31ae7c72c
Andrey Chorniy <creatuseandr@icloud.com>
2022-06-29 16:31:59 +0300

    created main page of planes

--- client/src/components/planeItem/PlaneItem.jsx
+++ client/src/components/planeItem/PlaneItem.jsx
@@ -1,10 +1,35 @@
-import React from 'react';
-import styles from './planeItem.module.css';
+import React from "react";
+import styles from "./planeItem.module.css";
+import { paths } from "../../paths";
+import { Link } from "react-router-dom";
 
-function PlaneItem() {
+function PlaneItem({
+  name = "",
+  price = 0,
+  capacity = "",
+  planeImage = "",
+  _id = "",
+}) {
+  console.log(planeImage);
+
   return (
-    <div>PlaneItem</div>
-  )
+    <Link
+      to={`${paths.plane}/${_id}`}
+      className={styles.planeItem}>
+      <div className={styles.capacity}>{capacity}</div>
+      {planeImage && (
+        <img
+          className={styles.image}
+          src={planeImage}
+          alt=''
+        />
+      )}
+      <div className={styles.info}>
+        <h2 className={styles.title}>{name}</h2>
+        <span className={styles.price}>{price}</span>
+      </div>
+    </Link>
+  );
 }
 
-export default PlaneItem
+export default PlaneItem;
