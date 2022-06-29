2aa1fbdb1071f06cbdc9b3675de5f705b94259eb
Andrey Chorniy <creatuseandr@icloud.com>
2022-06-29 19:15:38 +0300

    added navigation to main page for sorting planes by price

--- client/src/components/planes/Planes.jsx
+++ client/src/components/planes/Planes.jsx
@@ -6,12 +6,19 @@ import ContentWrapper from "../contentBlock/ContentWrapper";
 import Spinner from "../loader/Spinner";
 import PlaneItem from "../planeItem/PlaneItem";
 import styles from "./planes.module.css";
+import { Link } from "react-router-dom";
+import { paths } from "../../paths";
+import Button from "../button/Button";
+import { useSortItems } from "../../sortableHook/useSortItems";
 
 function Planes() {
   const dispatch = useDispatch();
   const { planes, isLoading } = useSelector((state) => {
     return state.planes;
   });
+  const { desc, setDesc, sortedItems } = useSortItems(
+    planes || [],
+  );
 
   console.log("planes >>>", planes);
 
@@ -24,12 +31,28 @@ function Planes() {
   }
 
   return (
-    <ContentWrapper className={styles.wrap}>
-      {planes &&
-        planes.map((plane) => (
-          <PlaneItem key={plane._id} {...plane} />
-        ))}
-    </ContentWrapper>
+    <div>
+      <div className={styles.sort}>
+        <ContentWrapper className={styles.planesNav}>
+          <Button
+            className={styles.sortBtn}
+            onClick={() => setDesc(!desc)}>
+            Сортировать по цене:{`${desc ? "по убыванию" : "по возрастанию"}`}
+          </Button>
+          <Link
+            to={paths.createPlane}
+            className={styles.createPlaneLink}>
+            Добавить самолет
+          </Link>
+        </ContentWrapper>
+      </div>
+      <ContentWrapper className={styles.wrap}>
+        {sortedItems &&
+          sortedItems.map((plane) => (
+            <PlaneItem key={plane._id} {...plane} />
+          ))}
+      </ContentWrapper>
+    </div>
   );
 }
 
