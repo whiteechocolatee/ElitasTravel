5e92eb8aa93de9e31defa8685904d249c3b469af
Andrey Chorniy <creatuseandr@icloud.com>
2022-06-29 19:11:37 +0300

    created sortableHook which sorting array of items

---
+++ client/src/sortableHook/useSortItems.js
@@ -0,0 +1,24 @@
+import { useMemo, useState } from "react";
+
+export const useSortItems = (planes = []) => {
+  const [desc, setDesc] = useState(false);
+
+  const sortedItems = useMemo(() => {
+    const sortableItems = [...planes];
+
+    sortableItems.sort((a, b) => {
+      if (+a.price < +b.price) return desc ? 1 : -1;
+      if (+a.price > +b.price) return desc ? -1 : 1;
+
+      return 0;
+    });
+
+    return sortableItems;
+  }, [desc, planes]);
+
+  return {
+    desc,
+    setDesc,
+    sortedItems,
+  };
+};
