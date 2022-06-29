9c225b39e9427178eecfcb8b8aee29822b30a66c
Andrey Chorniy <creatuseandr@icloud.com>
2022-06-29 13:58:16 +0300

    changed spinner logic and add condition on plane page

--- client/src/components/loader/Spinner.jsx
+++ client/src/components/loader/Spinner.jsx
@@ -4,17 +4,9 @@ import { useSelector } from "react-redux";
 import styles from "./spinner.module.css";
 
 function Spinner() {
-  const { isLoading } = useSelector((state) => {
-    return state.planes;
-  });
-
   return (
     <div className={styles.spinner}>
-      <ThreeDots
-        color='#66A1C7'
-        width='100'
-        visible={isLoading}
-      />
+      <ThreeDots color='#66A1C7' width='100' />
     </div>
   );
 }
