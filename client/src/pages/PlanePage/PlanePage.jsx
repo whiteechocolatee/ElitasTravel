f40010ad53d8d15a6f3b527a1788211e43c09ef7
Andrey Chorniy <creatuseandr@icloud.com>
2022-06-30 14:56:57 +0300

    connect to planePage some data from store for showing at main page

--- client/src/pages/PlanePage/PlanePage.jsx
+++ client/src/pages/PlanePage/PlanePage.jsx
@@ -1,8 +1,29 @@
-import React from "react";
-import { useParams } from "react-router-dom";
+import React, { useEffect } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { getPlane } from "../../store/plane/planeSlice";
+import Spinner from "../../components/loader/Spinner";
 
 function PlanePage() {
   const { id } = useParams();
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const { plane, isLoading } = useSelector(
+    (state) => state.plane,
+  );
+
+  console.log("id >>>", id);
+  console.log("plane >>>", plane);
+  console.log("load >>>", isLoading);
+
+  useEffect(() => {
+    dispatch(getPlane(id));
+  }, [dispatch, id]);
+
+  if (isLoading) {
+    return <Spinner />;
+  }
+
   return <div>{id}</div>;
 }
 
