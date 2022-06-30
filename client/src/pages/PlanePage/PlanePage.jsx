111be7eb0223113199088f2246d4776708f8167d
Andrey Chorniy <creatuseandr@icloud.com>
2022-06-30 15:26:49 +0300

    added plane data to plane page and added some styles for that

--- client/src/pages/PlanePage/PlanePage.jsx
+++ client/src/pages/PlanePage/PlanePage.jsx
@@ -3,6 +3,9 @@ import { useNavigate, useParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { getPlane } from "../../store/plane/planeSlice";
 import Spinner from "../../components/loader/Spinner";
+import ContentWrapper from "../../components/contentBlock/ContentWrapper";
+import styles from "./panePage.module.css";
+import Button from "../../components/button/Button";
 
 function PlanePage() {
   const { id } = useParams();
@@ -24,7 +27,34 @@ function PlanePage() {
     return <Spinner />;
   }
 
-  return <div>{id}</div>;
+  return (
+    plane && (
+      <ContentWrapper className={styles.plane}>
+        <div className={styles.descContent}>
+          <Button onClick={() => navigate(-1)}>
+            Назад
+          </Button>
+          <h1 className={styles.title}>{plane.name}</h1>
+          <div className={styles.price}>{plane.price}</div>
+          <Button
+            containerClassName={styles.btnBuy}
+            onClick={() => navigate("/order")}>
+            Добавить в корзину
+          </Button>
+          <p className={styles.planeDesc}>
+            {plane.description}
+          </p>
+        </div>
+        <div className={styles.imageContent}>
+          <img
+            className={styles.image}
+            src={plane.planeImage}
+            alt=''
+          />
+        </div>
+      </ContentWrapper>
+    )
+  );
 }
 
 export default PlanePage;
