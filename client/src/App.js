cc73b05ab6fd5f0bbe022709f4f96a4ace92dab2
Andrey Chorniy <creatuseandr@icloud.com>
2022-06-30 14:10:09 +0300

    created plane page for getting item info by id from database

--- client/src/App.js
+++ client/src/App.js
@@ -5,12 +5,17 @@ import {
   Route,
 } from "react-router-dom";
 import { paths } from "./paths";
+import PlanePage from "./pages/PlanePage/PlanePage";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path={paths.home} element={<Homepage />} />
+        <Route
+          path={`${paths.plane}/:id`}
+          element={<PlanePage />}
+        />
       </Routes>
     </BrowserRouter>
   );
