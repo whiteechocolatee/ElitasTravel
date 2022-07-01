1bd4a9159f0b4c3919fc177a6fafc04ae9a1d407
Andrey Chorniy <creatuseandr@icloud.com>
2022-07-01 17:06:03 +0300

    add new cases to create plane event

--- client/src/store/plane/planeSlice.js
+++ client/src/store/plane/planeSlice.js
@@ -17,6 +17,19 @@ export const getPlane = createAsyncThunk(
   },
 );
 
+export const createPlane = createAsyncThunk(
+  "createPlane",
+  async (formData, thunkAPI) => {
+    try {
+      return await planeService.createPlane(formData);
+    } catch (error) {
+      return thunkAPI.rejectWithValue(
+        error.response.message,
+      );
+    }
+  },
+);
+
 const planeSlice = createSlice({
   name: "plane",
   initialState: {
@@ -24,6 +37,7 @@ const planeSlice = createSlice({
     isLoading: false,
     isError: true,
     message: "",
+    errors: null,
   },
   extraReducers: (builder) => {
     builder
@@ -41,6 +55,22 @@ const planeSlice = createSlice({
         state.isLoading = false;
         state.isError = action.error.message;
         state.plane = null;
+      })
+
+      .addCase(createPlane.pending, (state) => {
+        state.isLoading = true;
+        state.errors = null;
+      })
+
+      .addCase(createPlane.fulfilled, (state) => {
+        state.isLoading = false;
+        state.errors = null;
+      })
+
+      .addCase(createPlane.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
+        state.errors = action.payload;
       });
   },
 });
