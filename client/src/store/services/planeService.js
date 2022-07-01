623bcfa86282b3770be56767bad73ac1a05a7d69
Andrey Chorniy <creatuseandr@icloud.com>
2022-07-01 17:04:12 +0300

    was created new plane service for creating new plane

--- client/src/store/services/planeService.js
+++ client/src/store/services/planeService.js
@@ -19,9 +19,22 @@ const getPlane = async (id) => {
   return plane.data;
 };
 
+/**
+ * It takes in a formData object, sends it to the server, and returns the data that the server sends
+ * back
+ * @param formData - This is the data that we're sending to the server.
+ * @returns The plane object
+ */
+const createPlane = async (formData) => {
+  const plane = await axios.post(`/api/planes/`, formData);
+
+  return plane.data;
+};
+
 const planeService = {
   getPlanes,
   getPlane,
+  createPlane,
 };
 
 export default planeService;
